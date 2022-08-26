204ffe41cb540e17ef56f51a07fbb2370db8cdea
Dell <Dell@DESKTOP-7CMUAMJ>
2022-08-26 18:10:01 +0530

    Assignment 2 program1

---
+++ javapro/src/main/java/javapro/Numb.java
@@ -0,0 +1,18 @@
+package javapro;
+
+public class Numb {//find hcf of two number
+
+	public static void main(String[] args) {
+
+		int num1 = 60, num2 = 36, hcf=0;
+
+	    for (int i = 1; i <= num1 || i <= num2; i++)
+	      {
+	     if (num1 % i == 0 && num2 % i == 0)
+	        hcf = i;
+	      }
+
+	    System.out.println("The HCF: "+ hcf);
+	}
+
+}
