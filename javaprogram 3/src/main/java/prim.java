d5795018955b958c012fb7e5d5e7e2447c0228a4
Dell <Dell@DESKTOP-7CMUAMJ>
2022-09-01 17:57:27 +0530

    project 3.1

---
+++ javaprogram 3/src/main/java/prim.java
@@ -0,0 +1,19 @@
+
+public class prim {
+
+	public static void main(String[] args) {
+		for(int n=100; n<=200; n++) {
+			int temp=0;
+			for (int i=2; i<=n-1;i++) {
+				if (n%i==0) {
+					temp=temp+1;
+				}
+			}
+			if(temp==0) {
+				System.out.println(n);
+			}
+		}
+
+	}
+
+}
